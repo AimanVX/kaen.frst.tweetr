a042b1a3cd71b05adf90ee521114b2cc3331e233
AIMAN  BEN AWEG <athur20005@gmail.com>
2026-01-22 01:37:05 +0300

    Update start.sh
    3when serching free server

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+php artisan config:clear || true
+php artisan cache:clear || true
+
+
 sed -i "s/Listen 80/Listen ${PORT:-8080}/" /etc/apache2/ports.conf || true
 sed -i "s/:80>/:${PORT:-8080}>/" /etc/apache2/sites-available/000-default.conf || true
 
