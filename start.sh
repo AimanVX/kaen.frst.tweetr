7f11708f5abfcb3f081a957c7ef711281f58cc73
AIMAN  BEN AWEG <athur20005@gmail.com>
2026-01-22 00:23:12 +0300

    Create start.sh
    2 when serching free

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+sed -i "s/Listen 80/Listen ${PORT:-8080}/" /etc/apache2/ports.conf || true
+sed -i "s/:80>/:${PORT:-8080}>/" /etc/apache2/sites-available/000-default.conf || true
+
+php artisan config:cache || true
+php artisan route:cache || true
+php artisan view:cache || true
+php artisan migrate --force || true
+
+apache2-foreground
